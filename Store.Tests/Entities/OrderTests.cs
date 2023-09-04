d7f503e25b638f2711e48099f50148aafc3c285b
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-09-04 19:52:53 -0300

    Testando o Status do Pedido

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -6,13 +6,24 @@ namespace Store.Tests.Domain
     [TestClass]
     public class OrderTests
     {
+        private readonly Customer _customer = new Customer("André Baltieri", "andre@balta.io");
+        private readonly Product _product = new Product("Produto 1", 10, true);
+        private readonly Discount _discount = new Discount(10, DateTime.Now.AddDays(5));
+
         [TestMethod]
         [TestCategory("Domain")]
         public void DadoUmNovoPedidoValidoEleDeveGerarUmNumeroComOitoCaracteres()
         {
-            var customer = new Customer("André Baltieri", "andre@balta.io");
-            var order = new Order(customer, 0, null);
-            Assert.AreEqual(8, order.Number.Lenght);
+            var order = new Order(_customer, 0, null);
+            Assert.AreEqual(8, order.Number.Length);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void DadoUmNovoPedidoSeuStatusDeveSerAguardandoPagamento()
+        {
+            var order = new Order(_customer, 0, null);
+            Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
         }
     }
 }
